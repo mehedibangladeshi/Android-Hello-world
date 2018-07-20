c1efce914b64f4c367a1e1bfc63ff403fd455ca5
Mehedi Hasan <mehedipy@gmail.com>
2018-07-20 12:40:34 +0600

    initial commit

---
+++ app/src/main/java/com/mehedihasanbangladeshi/iiuc/sdmagp/helloworld/MainActivity.java
@@ -0,0 +1,13 @@
+package com.mehedihasanbangladeshi.iiuc.sdmagp.helloworld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
